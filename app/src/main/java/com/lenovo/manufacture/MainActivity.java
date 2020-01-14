dfc35181f74bb1a5d7ebceee4464d99a9ab35533
AYuan <lzy2118053626@hotmaill.com>
2020-01-14 11:45:42 +0800

    在app的model中加入了GridView，用于跳转到其他model

--- app/src/main/java/com/lenovo/manufacture/MainActivity.java
+++ app/src/main/java/com/lenovo/manufacture/MainActivity.java
@@ -1,7 +1,18 @@
 package com.lenovo.manufacture;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 import com.lenovo.basic.base.act.BaseActivity;
 import com.lenovo.basic.utils.Network;
@@ -20,14 +31,41 @@ import io.reactivex.schedulers.Schedulers;
  */
 public class MainActivity extends BaseActivity {
 
-    @Override
-    protected void initEvent() {
 
+    private GridView gv_menu;
+    private GridViewAdater gridViewAdater;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
     }
 
+    @Override
+    protected int getLayoutIdRes() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+        gv_menu = (GridView) findViewById(R.id.gv_menu);
+    }
+
+    @Override
+    protected void initEvent() {
+        gv_menu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
 
     @Override
     protected void initData() {
+        // 给GridView设置数据适配器
+        gridViewAdater = new GridViewAdater();
+        gv_menu.setAdapter(gridViewAdater);
+
         ApiService apiService = Network.remote(ApiService.class);
         Disposable disposable = apiService.getMaterial()
                 .subscribeOn(Schedulers.io())//耗时线程
@@ -54,18 +92,42 @@ public class MainActivity extends BaseActivity {
         //disposable.dispose();
     }
 
-    @Override
-    protected int getLayoutIdRes() {
-        return R.layout.activity_main;
+    class GridViewAdater extends BaseAdapter {
+        private ImageView ivIcon;
+        private TextView tvTitle;
+
+        @Override
+        public int getCount() {
+            return 23;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+            if (convertView == null) {
+                view = View.inflate(MainActivity.this, R.layout.item_menu, null);
+            } else {
+                view = convertView;
+            }
+            initView(view);
+            tvTitle.setText(position + 1 + "");
+            return view;
+        }
+
+        private void initView(View view) {
+            ivIcon = (ImageView) view.findViewById(R.id.iv_icon);
+            tvTitle = (TextView) view.findViewById(R.id.tv_title);
+        }
     }
 
-    private TextView mTv;
-
-    @Override
-    protected void initView() {
-
-        mTv = (TextView) findViewById(R.id.tv);
-
-
-    }
 }
--- basic/src/main/java/com/lenovo/basic/base/act/BaseActivity.java
+++ basic/src/main/java/com/lenovo/basic/base/act/BaseActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 
 import androidx.annotation.LayoutRes;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
