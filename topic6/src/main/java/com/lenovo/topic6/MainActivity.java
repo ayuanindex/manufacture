52951827be5af9ee73b2a56347c686d506be0ead
AYuan <lzy2118053626@hotmaill.com>
2020-01-18 18:38:02 +0800

    Create module topic6

---
+++ topic6/src/main/java/com/lenovo/topic6/MainActivity.java
@@ -0,0 +1,42 @@
+package com.lenovo.topic6;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+
+import com.lenovo.basic.base.act.BaseActivity;
+
+/**
+ * @ProjectName: manufacture
+ * @Package: com.lenovo.topic6
+ * @ClassName: MainActivity
+ * @Author: AYuan
+ * @CreateDate: 2020/1/18 18:34
+ */
+public class MainActivity extends BaseActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected int getLayoutIdRes() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initEvent() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
