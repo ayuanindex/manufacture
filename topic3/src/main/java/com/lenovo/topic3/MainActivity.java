bca1ecbbd2f90889cc9603e73d8b3df0bdb88468
AYuan <lzy2118053626@hotmaill.com>
2020-02-04 17:01:22 +0800

    更新了topic3的注释

--- topic3/src/main/java/com/lenovo/topic3/MainActivity.java
+++ topic3/src/main/java/com/lenovo/topic3/MainActivity.java
@@ -37,11 +37,19 @@ public class MainActivity extends BaseActivity {
     private ApiService remote;
     private ArrayList<Disposable> disposables;
 
+    /**
+     * 获取布局文件
+     *
+     * @return 返回布局文件的ID
+     */
     @Override
     protected int getLayoutIdRes() {
         return R.layout.activity_main;
     }
 
+    /**
+     * 初始化控件
+     */
     protected void initView() {
         iv_back = (ImageView) findViewById(R.id.iv_back);
         tv_electricity = (TextView) findViewById(R.id.tv_electricity);
@@ -50,6 +58,9 @@ public class MainActivity extends BaseActivity {
         sb_changeValue = (SeekBar) findViewById(R.id.sb_changeValue);
     }
 
+    /**
+     * 初始化监听
+     */
     @Override
     protected void initEvent() {
         iv_back.setOnClickListener(new View.OnClickListener() {
@@ -86,6 +97,9 @@ public class MainActivity extends BaseActivity {
         });
     }
 
+    /**
+     * 声明实例化需要使用的工具类
+     */
     @Override
     protected void initData() {
         // 实例话ApiService接口
@@ -101,9 +115,12 @@ public class MainActivity extends BaseActivity {
      */
     private void setFactoryPowerConsumption() {
         Disposable subscribe = remote.setFactoryEnvironment(1, currentProgress)
+                // 切换到子线程进行网络请求
                 .subscribeOn(Schedulers.io())
+                // 切换到主线程对整理好的数据进行展示
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<UpdatePower>() {
+                // 订阅网络请求的状体
+                .subscribe(new Consumer<UpdatePower>() {// 请求成功
                     @SuppressLint("SetTextI18n")
                     @Override
                     public void accept(UpdatePower updatePower) throws Exception {
@@ -113,12 +130,13 @@ public class MainActivity extends BaseActivity {
                         tv_power_consumption.setText(updatePower.getData().getPower() + "kw/h");
                         rsb_ruler.setCurrentProgress(Integer.parseInt(updatePower.getData().getPower()));
                     }
-                }, new Consumer<Throwable>() {
+                }, new Consumer<Throwable>() {// 请求失败
                     @Override
                     public void accept(Throwable throwable) throws Exception {
                         Log.i(TAG, "哈哈：" + throwable.getMessage());
                     }
                 });
+        // 将订阅天较大订阅集合中，待到界面销毁时随界面一起销毁
         disposables.add(subscribe);
     }
 
@@ -126,25 +144,35 @@ public class MainActivity extends BaseActivity {
      * 获取工厂用电量的网络请请求方法
      */
     private void getFactoryPowerConsumption() {
+        // 定时获取工厂用电量
+        // @field1：第一次执行的延时；@field2：循环周期的间隔时间；@field3：时间的计时单位
         Disposable subscribe = Observable.interval(0, 5, TimeUnit.SECONDS)
+                // 周期循环时所执行的方法
                 .doOnNext(new Consumer<Long>() {
 
+                    /**
+                     * 获取工厂用电量的订阅
+                     */
                     private Disposable subscribe;
 
                     @Override
                     public void accept(Long aLong) throws Exception {
+                        // 判断订阅是否存在以及是否已经处于订阅状态，如果是的话则将订阅取消
                         if (subscribe != null && !subscribe.isDisposed()) {
                             Log.i(TAG, "哈哈：" + aLong);
                             subscribe.dispose();
                         }
                         subscribe = remote.getFactoryEnvironment(1)
+                                // 切换到子线程有运行
                                 .subscribeOn(Schedulers.io())
+                                // 对获取到的数据进行变换，拿到自己想要的数据在接下来的方法中呢继续使用
                                 .map(new Function<FactoryEnvironment, FactoryEnvironment.DataBeanList>() {
                                     @Override
                                     public FactoryEnvironment.DataBeanList apply(FactoryEnvironment factoryEnvironment) throws Exception {
                                         return factoryEnvironment.getData().get(0);
                                     }
                                 })
+                                // 切换到主线程运行
                                 .observeOn(AndroidSchedulers.mainThread())
                                 .subscribe(new Consumer<FactoryEnvironment.DataBeanList>() {
                                     @SuppressLint("SetTextI18n")
